ae0080e5d95754d22a8c37f693ebbde9c5801e26
fasolksa <ftyy2015@hotmail.com>
2024-11-25 11:54:25 +0300

    1

---
+++ midt/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+//faisal alyahya
+/* 
+class Q1  
+{
+    static void Main()
+    {
+        string name = ("Faisal");
+        int age = 25;
+        string grade = ("97");
+        
+        Console.WriteLine("Name:"+name);
+        Console.WriteLine("Age:"+age);
+        Console.WriteLine("grade:"+grade);
+    }
+}
+*/
+
+/*
+class Q3  
+{
+    
+    static void Main()
+    {
+        
+        Console.WriteLine("enter your number:");
+        int i =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(i*i);
+
+    }
+}
+*/
+/*
+class Q4  
+{
+    
+    static void Main()
+    {
+        
+        Console.WriteLine("enter your number:");
+        int i =Convert.ToInt32(Console.ReadLine());
+        if(i < 0)
+        {
+            Console.WriteLine("Negative");
+        }
+        else if (i > 0)
+        {
+            Console.WriteLine("Positive");
+        }
+    
+        else
+        {
+            Console.WriteLine("Zero");
+        }
+    }
+}
+*/
+/*
+class Q6  
+{
+    static void Main()
+    {
+        string name = ("Faisal");
+        int age = 25;
+        double wage = 25.3;
+        
+        Console.WriteLine($"Hi, my name is {name} . I am {age} years old and earn ${wage} per hour");
+        
+    }
+}
+*/
+
+/* Q car didn't complete
+class Car
+{
+    string make;
+    string model;
+    int year;
+    string color;
+    
+}
+class Qcar  
+{
+    
+    static void Main()
+    {
+        
+    }
+}
+*/
